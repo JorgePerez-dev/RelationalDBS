dd56a7fbe9cb2583de75a57be5b5da74a49e38d5
Jorge Perez <jorgeimon100@gmail.com>
2026-04-09 11:01:04 +0200

    primer commit

---
+++ lib/src/main/java/org/slf4j/LoggerFactory.java
@@ -0,0 +1,13 @@
+package org.slf4j;
+
+import relationaldbs.Logger;
+import relationaldbs.LogginTest;
+
+public class LoggerFactory {
+
+	public static Logger getLogger(Class<LogginTest> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
