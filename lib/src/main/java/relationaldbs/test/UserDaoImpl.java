d5736f8be66b37b23b044fb17d3603a1eed883b9
Jorge Perez <jorgeimon100@gmail.com>
2026-04-17 09:15:47 +0200

    actualizado

--- lib/src/main/java/relationaldbs/test/UserDaoImpl.java
+++ lib/src/main/java/relationaldbs/test/UserDaoImpl.java
@@ -1,6 +1,8 @@
 package relationaldbs.test;
 
 
+import java.util.List;
+
 import relationaldbs.user;
 import relationaldbs.dao.UserDao;
 
@@ -10,6 +12,36 @@ public class UserDaoImpl implements UserDao {
 		UserDao userDao =  (UserDao) new UserDaoImpl();
 		userDao.insert(new user("Jorge", "18", "jorge@email", "7463746373", "madrid", "españa", null, null, 0));
 		
+	}
+
+	@Override
+	public boolean insert(user user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update(user user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public user find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<user> findall() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- lib/src/main/java/relationaldbs/user.java
+++ lib/src/main/java/relationaldbs/user.java
@@ -5,7 +5,7 @@ public class user {
     // Fields
     private String name;
     private String password;
-    private int age;
+    private String age;
     private String email;
     private String phone;
     private String address;
@@ -32,7 +32,7 @@ public class user {
 
     public String getName() { return name; }
     public String getPassword() { return password; }
-    public int getAge() { return age; }
+    public String getAge() { return age; }
     public String getEmail() { return email; }
     public String getPhone() { return phone; }
     public String getAddress() { return address; }
@@ -44,7 +44,6 @@ public class user {
 
     public void setName(String name) { this.name = name; }
     public void setPassword(String password) { this.password = password; }
-    public void setAge(int age) { this.age = age; }
     public void setEmail(String email) { this.email = email; }
     public void setPhone(String phone) { this.phone = phone; }
     public void setAddress(String address) { this.address = address; }
